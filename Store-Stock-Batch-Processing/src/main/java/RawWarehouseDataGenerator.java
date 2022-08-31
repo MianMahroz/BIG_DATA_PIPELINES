3d46b4264b2acd040681fad0dd7dcd03593df452
mahroz.naeem <mianmahroz1@gmail.com>
2022-08-31 21:02:51 +0400

    Raw data generator abstract

---
+++ Store-Stock-Batch-Processing/src/main/java/RawWarehouseDataGenerator.java
@@ -0,0 +1,26 @@
+/**
+ * This class is responsible to generate raw data for local warehouses
+ * So we can use it later in Data pipelines
+ */
+public class RawWarehouseDataGenerator {
+
+    private void openMariaDbConnection(){
+
+    }
+
+    private void closeDbConnection(){
+
+    }
+
+    private void setUpWareHouses(){
+
+    }
+
+    private void readFromWareHouse() {
+
+    }
+
+    private void writeToWareHouse(){
+
+    }
+}
