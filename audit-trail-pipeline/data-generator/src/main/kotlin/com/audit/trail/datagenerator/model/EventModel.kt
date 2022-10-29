a8bb5373c2659b9d22656adf2c807f392ec9b493
Mian Mahroz Naeem <mianmahroz1@gmail.com>
2022-10-29 14:12:57 +0500

    model and config updates for mongo cluster with replica config

--- audit-trail-pipeline/data-generator/src/main/kotlin/com/audit/trail/datagenerator/model/EventModel.kt
+++ audit-trail-pipeline/data-generator/src/main/kotlin/com/audit/trail/datagenerator/model/EventModel.kt
@@ -4,6 +4,6 @@ import com.audit.trail.datagenerator.util.EventTypes
 import org.springframework.data.annotation.Id
 import org.springframework.data.mongodb.core.mapping.Document
 
-@Document(value = "audit_log")
+@Document(value = "auditlog")
 class EventModel(@Id var id:Long, var type:EventTypes,var details:Any) {
 }
